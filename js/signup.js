6149ac4787f056a9c40331130260250c06d51be6
emnetag <emnetg@uw.edu>
2014-10-22 10:14:04 -0700

    created a function for validating birthdates onReady function now includes all other event listeners and functions

--- js/signup.js
+++ js/signup.js
@@ -1,14 +1,11 @@
-/*
-    Signup Form Script
-    This script will load the state select list and validate the form before submission
-*/
-var signupForm = document.getElementById('signup');
-var occupationOther = signupForm.elements['occupationOther'];
-var occupationSelect = signupForm.elements['occupation'];
-var requiredFields = ['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate'];
+document.addEventListener('DOMContentLoaded', onReady);
 
+function onReady() {
+    var signupForm = document.getElementById('signup');
+    var occupationOther = signupForm.elements['occupationOther'];
+    var occupationSelect = signupForm.elements['occupation'];
+    var requiredFields = ['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate'];
 
-document.addEventListener('DOMContentLoaded', function() {
     var stateSelect = signupForm.elements['state'];
 
     var cancelButton = document.getElementById('cancelButton');
@@ -23,13 +20,14 @@ document.addEventListener('DOMContentLoaded', function() {
         stateSelect.appendChild(option);
     }
 
-    occupationSelect.addEventListener('change', function() {
+    occupationSelect.addEventListener('change', function () {
         if (occupationSelect.value == 'other') {
             occupationOther.style.display = "inline";
         }
         else {
             occupationOther.style.display = "none";
         }
+
     });
 
     cancelButton.addEventListener('click', function () {
@@ -39,65 +37,62 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     signupForm.addEventListener('submit', onSubmit);
-});
 
-function onSubmit(evt) {
-    evt.returnValue = validateForm(this);
-    if(!evt.returnValue && evt.preventDefault) {
-        evt.preventDefault();
-    }
-    return evt.returnValue;
-}
-
-function validateForm(form) {
-    var idx;
-    var formValid = true;
-
-    if (occupationSelect.value == 'other') {
-        requiredFields.push('occupationOther');
-    }
-
-    for (idx = 0; idx < requiredFields.length; ++idx) {
-        formValid &= validateRequiredField(form.elements[requiredFields[idx]]);
-    }
-
-    return formValid;
-}
-
-function validateRequiredField(field) {
-    var value = field.value.trim();
-    var valid;
-
-    if (field.name == 'zip') {
-        var zipRegExp = new RegExp('^\\d{5}$');
-        valid = zipRegExp.test(value);
-    } else if (field.name == 'birthdate') {
-        var currentDate = new Date();
-        var dateValue = new Date(value);
-
-       if (currentDate.getFullYear() - dateValue.getFullYear() > 13) {
-           valid = true;
-       } else if (currentDate.getFullYear() - dateValue.getFullYear() == 13) {
-           valid = (currentDate.getMonth() >= dateValue.getUTCMonth()) && (currentDate.getDate() >= dateValue.getUTCDate());
-       } else {
-           valid = false;
-    }
-    } else {
-        valid = value.length > 0;
-    }
-
-    if (valid) {
-        field.className = 'form-control';
-    }
-    else {
-        if (field.name = 'birthdate') {
-            var birthdateMessage = document.getElementById('birthdateMessage');
-            birthdateMessage.innerHTML = 'Must be 13 years or older to sign up!';
+    function onSubmit() {
+        evt.returnValue = validateForm(this);
+        if (!evt.returnValue && evt.preventDefault) {
+            evt.preventDefault();
         }
-        field.className = 'form-control invalid-field';
     }
-    return valid;
+
+    function validateForm(form) {
+        var idx;
+        var formValid = true;
+
+        if (occupationSelect.value == 'other') {
+            requiredFields.push('occupationOther');
+        }
+
+        for (idx = 0; idx < requiredFields.length; ++idx) {
+            formValid &= validateRequiredField(form.elements[requiredFields[idx]]);
+        }
+    }
+
+    function validateRequiredField(field) {
+        var value = field.value.trim();
+        var valid;
+
+        if (field.name == 'zip') {
+            var zipRegExp = new RegExp('^\\d{5}$');
+            valid = zipRegExp.test(value);
+        } else if (field.name == 'birthday') {
+            var dateValue = new Date(value);
+            valid = validateBirthDate(dateValue);
+        } else {
+            valid = value.length > 0;
+        }
+
+        if (!valid) {
+            if (field.name == 'birthdate') {
+                var birthdateMessage = document.getElementById('birthdateMessage');
+                birthdateMessage.innerHTML = 'You must be 13 years or older to sign up';
+            }
+            field.className = 'form-control invalid';
+        }
+        else {
+            field.className = 'form-control';
+        }
+    }
+
+    function validateBirthDate(dateValue) {
+        if (currentDate.getFullYear() - dateValue.getFullYear() > 13) {
+            valid = true;
+        } else if (currentDate.getFullYear() - dateValue.getFullYear() == 13) {
+            valid = (currentDate.getMonth() >= dateValue.getUTCMonth()) && (currentDate.getDate() >= dateValue.getUTCDate());
+        } else {
+            valid = false;
+        }
+        return valid;
+    }
 }
-
-
 
